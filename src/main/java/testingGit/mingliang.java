30d11b0ed21e06bf30396b7f776b0b1af06f3d56
test1 <test@test.com>
2018-05-25 14:36:14 +0800

    ww

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -12,7 +12,7 @@ public class mingliang {
 		System.out.println("JJ SHA bbb");
 		System.out.println("JJ SHA bbb");
 		System.out.println("JJ SHA bbb");
-		System.out.println("JJ SHA bbb");
+		System.out.println("JJ SHA bbba");
 	}
 	
 	public void hiMingLiang111(){}
