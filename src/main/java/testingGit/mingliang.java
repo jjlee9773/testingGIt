6b3cb68205cec20b5f229c5d8bea0bed294c1ad0
test1 <test@test.com>
2018-05-25 14:51:39 +0800

    asas

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -12,7 +12,7 @@ public class mingliang {
 		System.out.println("JJ SHA bbb");
 		System.out.println("JJ SHA bbb");
 		System.out.println("JJ SHA bbb");
-		System.out.println("JJ SHA bbbaaaa");
+		System.out.println("JJ SHA bbbccccc");
 	}
 	
 	public void hiMingLiang111(){}
