8d8d2675292f97c6ee2a7b23ea36aa337e3d4492
test1 <test@test.com>
2018-05-25 12:08:01 +0800

    test

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -3,6 +3,7 @@ package testingGit;
 public class mingliang {
 	public void hiMingLiang(){
 		System.out.println("JJ SHA bbb");
+		System.out.println("bug fix");
 	}
 	
 	public void hiMingLiang111(){}
