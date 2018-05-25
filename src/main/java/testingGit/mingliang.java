9e358a773012c28e4d7be011d6f8645f8fd02714
test1 <test@test.com>
2018-05-25 11:51:01 +0800

    change 2

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -2,7 +2,7 @@ package testingGit;
 
 public class mingliang {
 	public void hiMingLiang(){
-		System.out.println("JJ SHA b");
+		System.out.println("JJ SHA bbb");
 	}
 	
 	public void hiMingLiang111(){}
