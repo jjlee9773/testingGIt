a1a0c7f025dd0ac4249723db4605a8a3bf35206a
JJ <kokhau.lee@gswhouse.com>
2018-05-31 17:01:04 +0800

    testing change

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -2,17 +2,17 @@ package testingGit;
 
 public class mingliang {
 	public void hiMingLiang(){
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbbcccccd new update1");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("cccccd new update1");
 	}
 	
 	public void hiMingLiang111(){}
