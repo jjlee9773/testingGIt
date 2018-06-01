26980f78b7b5b6c94bc3638413eaa48be5f2c442
haha <haha@gmail.com>
2018-06-01 17:27:29 +0800

    jj

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -14,7 +14,7 @@ public class mingliang {
 		System.out.println("chee Hong SHA bbbxxxxxx");
 		System.out.println("chee Hong SHA bbbcccccd new update1222");
 		System.out.println("my testing 1 add");
-		System.out.println("my testing 1 abdd jjjjjjjjjjjjjjjj");
+		System.out.println("my testing 1 abdd jjjjjjjjjjjjjjjj cbcbcbcbcbcbcbcb");
 	}
 	
 	public void hiMingLiang111(){}
