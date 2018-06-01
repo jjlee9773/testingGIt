7055fe7550d56c29a7009df79b6df618f3737867
JJ <kokhau.lee@gswhouse.com>
2018-06-01 17:29:30 +0800

    merging

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -13,8 +13,8 @@ public class mingliang {
 		System.out.println("chee Hong SHA bbb");
 		System.out.println("chee Hong SHA bbbxxxxxx");
 		System.out.println("chee Hong SHA bbbcccccd new update1222");
-		System.out.println("my testing 1 add");
-		System.out.println("my testing 1 abdd jjjjjjjjjjjjjjjj cbcbcbcbcbcbcbcb tt");
+		System.out.println("hi Ming Liang ");
+		System.out.println("my testing 1 abdd jjjjjjjjjjjjjjjj cbcbcbcbcbcbcbcb");
 	}
 	
 	public void hiMingLiang111(){}
