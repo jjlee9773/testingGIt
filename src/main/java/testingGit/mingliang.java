f834abbd153846ecf720a50594b72cf1b6c354bb
haha <haha@gmail.com>
2018-06-01 18:06:14 +0800

    commit merge

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -3,6 +3,7 @@ package testingGit;
 public class mingliang {
 	public void hiMingLiang(){
 		System.out.println("don't play play!!!!!!");
+		System.out.println("don't play play!!!!!");
 		System.out.println("don't play play!!!!!!");
 	}
 	
