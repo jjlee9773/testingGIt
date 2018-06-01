040f2ca8499f3469faed09773bae466d8a5fc08e
JJ <kokhau.lee@gswhouse.com>
2018-06-01 17:41:22 +0800

    testing commit

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -2,19 +2,7 @@ package testingGit;
 
 public class mingliang {
 	public void hiMingLiang(){
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbb");
-		System.out.println("chee Hong SHA bbbxxxxxx");
-		System.out.println("chee Hong SHA bbbcccccd new update1222");
-		System.out.println("hi Ming Liang ");
-		System.out.println("my testing 1 abdd jjjjjjjjjjjjjjjj cbcbcbcbcbcbcbcb");
+		System.out.println("don't play play");
 	}
 	
 	public void hiMingLiang111(){}
