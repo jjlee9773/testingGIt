7fc6546397944312fb2b99526f97fa13ecb8e1cf
test1 <test@test.com>
2018-06-01 18:12:31 +0800

    cc

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -2,9 +2,9 @@ package testingGit;
 
 public class mingliang {
 	public void hiMingLiang(){
+		System.out.println("don't play play!!!!!!");
 		System.out.println("don't play play!!!!!");
-		System.out.println("don't play play!!!!!");
-		System.out.println("don't play play!!!!!");
+		System.out.println("don't play play!!!!!!");
 	}
 	
 	public void hiMingLiang111(){}
