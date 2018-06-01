5a1942a2f00147d0ccae75a19cd11e90ef7b1516
haha <haha@gmail.com>
2018-06-01 17:04:24 +0800

    xx

--- src/main/java/testingGit/testingMingLiang.java
+++ src/main/java/testingGit/testingMingLiang.java
@@ -3,7 +3,7 @@ package testingGit;
 public class testingMingLiang {
 	public void hiMingLiang(){}
 	public void cheeHong(){
-		System.out.println("swsw oooo");
+		System.out.println("swsw 11oooo");
 	}
 
 	public void cheeHong2(){}
