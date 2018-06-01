b8346fd2007fec66b0949e950f39134f10f5c5f9
test1 <test@test.com>
2018-06-01 18:04:43 +0800

    testing commit

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -4,6 +4,7 @@ public class mingliang {
 	public void hiMingLiang(){
 		System.out.println("don't play play!!!!!!");
 		System.out.println("don't play play!!!!!!");
+		System.out.println("don't play play!!!!!");
 	}
 	
 	public void hiMingLiang111(){}
