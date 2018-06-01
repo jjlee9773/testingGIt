55fd5eefab58425901b4446b8a1d5b694e64e859
test1 <test@test.com>
2018-06-01 18:03:16 +0800

    testing rebase

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -3,6 +3,8 @@ package testingGit;
 public class mingliang {
 	public void hiMingLiang(){
 		System.out.println("don't play play!!!!!");
+		System.out.println("don't play play!!!!!");
+		System.out.println("don't play play!!!!!");
 	}
 	
 	public void hiMingLiang111(){}
