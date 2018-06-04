6649708e3872a6d2d51a92ac965dab8ebe7172e8
JJ <kokhau.lee@gswhouse.com>
2018-06-04 17:04:02 +0800

    sds

--- src/main/java/my/p/keKe.java
+++ src/main/java/my/p/keKe.java
@@ -4,12 +4,8 @@ public class keKe {
 	
 	public static void main(String agr[]){
 		System.out.print("KEKE IS WHO");
-<<<<<<< Upstream, based on branch 'master' of https://github.com/jjlee9773/testingGIt.git
-		System.out.print("KEKEKEKEKEKEKE whahahahahahahahahaha");
-=======
 		System.out.print("KEKEKEKEKEKEKE");
 		System.out.print("KEKEKEKEKEKEKE");
->>>>>>> efc58a4 sddsds
 	}
 
 }
