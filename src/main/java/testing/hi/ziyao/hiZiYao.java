008b4fbd482cb32efa7b040db3488b31fe177fc1
JJ <kokhau.lee@gswhouse.com>
2018-06-04 12:03:09 +0800

    ,,,

--- src/main/java/testing/hi/ziyao/hiZiYao.java
+++ src/main/java/testing/hi/ziyao/hiZiYao.java
@@ -2,12 +2,6 @@ package testing.hi.ziyao;
 
 public class hiZiYao {
 	public void hiZiyao(){
-		System.out.println("testing hi zi yaoaa");
-		System.out.println("testing hi zi yao");
-		System.out.println("testing hi zi yao");
-		System.out.println("testing hi zi yaoaa");
-		System.out.println("testing hi zi yao");
-		System.out.println("testing hi zi yao");
-		System.out.println("testing hi zi yao111");
+		System.out.println("testing hi zi212121 yao");
 	}
 }
