3f065497037c1ebe7c302732e480c89871cd6bd1
JJ <kokhau.lee@gswhouse.com>
2018-06-04 12:52:32 +0800

    ddd

--- src/main/java/testingGit/testingMingLiang.java
+++ src/main/java/testingGit/testingMingLiang.java
@@ -5,6 +5,7 @@ public class testingMingLiang {
 	public void cheeHong(){
 		System.out.println("swsw 11oooo");
 		System.out.println("hi mingliang cb zaivvv");
+		System.out.println("hi mingliang cb zaivvv");
 	}
 
 	public void cheeHong2(){}
