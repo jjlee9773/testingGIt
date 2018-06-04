1f68bca89ccdc9ec61345bed17c3eb71bca67420
haha <haha@gmail.com>
2018-06-04 12:58:40 +0800

    Revert "sssss"
    This reverts commit 2e542da7c5fc2ae53b497f69f414f3c623b9aafd.

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -10,5 +10,21 @@ public class mingliang {
 		System.out.println("don't play play!!!wdcwv!!!");
 	}
 	
+	public void hiMingLiang111(){
+		
+		System.out.println("bobo cccc dsdjjaaaaaaaa");
+	}
+	
+	public void hiMingLiang112(){
+		System.out.println("bobo MvvL");
+	}
+	
+	public void hiMingLiang113(){
+		System.out.println("bobo j");
+	}
+	
+	public void hiMingLiang114(){
+		
+	}
 }
 	
