cf240c25a8205a3dfbd41b8ec89f698282973cbd
test1 <test@test.com>
2018-06-04 11:17:39 +0800

    mmm

--- src/main/java/testing/hi/ziyao/hiZiYao.java
+++ src/main/java/testing/hi/ziyao/hiZiYao.java
@@ -6,7 +6,11 @@ public class hiZiYao {
 		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yaoaa");
+<<<<<<< HEAD
 		System.out.println("testing hi zi yao");
+=======
+		System.out.println("testing hi zi yao");
+>>>>>>> branch 'master' of https://github.com/jjlee9773/testingGIt.git
 		System.out.println("testing hi zi212121 yao");
 		System.out.println("testing hi zi yao1222333");
 	}
