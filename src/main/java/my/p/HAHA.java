d6ef0d47175d2a1b9831b03894546aa655635992
JJ <kokhau.lee@gswhouse.com>
2018-06-04 16:58:37 +0800

    wwww

--- src/main/java/my/p/HAHA.java
+++ src/main/java/my/p/HAHA.java
@@ -5,6 +5,7 @@ public class HAHA {
 		System.out.println("chee hong sha bi");
 		System.out.println("chee hong sha bi");
 		System.out.println("chee aaaaahong sha bi");
+		System.out.println("lol");
 		
 		System.out.println("lol");
 	}
