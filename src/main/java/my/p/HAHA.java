f4015595a9e6b93e67096052c55e90d815091d71
Wakanda Forever <test@test.com>
2018-06-04 17:34:09 +0800

    what is 69??????? black man question mark

--- src/main/java/my/p/HAHA.java
+++ src/main/java/my/p/HAHA.java
@@ -4,7 +4,7 @@ public class HAHA {
 	public static void main(String agrs[]){
 		System.out.println("chee hong sha bi");
 		System.out.println("chee hong sha bi");
-		System.out.println("chee hong sha bi");
+		System.out.println("chee hong sha bi cch");
 		
 		System.out.println("lol");
 	}
