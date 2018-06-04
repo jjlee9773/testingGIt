70373ddc6569bff98615137dd3b6313def1a0331
JJ <kokhau.lee@gswhouse.com>
2018-06-04 10:57:28 +0800

    111

--- src/main/java/testing/hi/ziyao/hiZiYao.java
+++ src/main/java/testing/hi/ziyao/hiZiYao.java
@@ -2,6 +2,8 @@ package testing.hi.ziyao;
 
 public class hiZiYao {
 	public void hiZiyao(){
+		System.out.println("testing hi zi yaoaa");
+		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yao");
 	}
