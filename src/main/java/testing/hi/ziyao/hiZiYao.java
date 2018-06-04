8e488bdc703cff852caefda58e57225c8415f383
JJ <kokhau.lee@gswhouse.com>
2018-06-04 12:19:19 +0800

    mmm

--- src/main/java/testing/hi/ziyao/hiZiYao.java
+++ src/main/java/testing/hi/ziyao/hiZiYao.java
@@ -8,6 +8,7 @@ public class hiZiYao {
 		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yaoaa");
 		System.out.println("testing hi zi yao1222333");
+	}
 	public void hiZiyao(String hihihi){
 	}
 }
