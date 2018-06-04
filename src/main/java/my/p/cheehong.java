4cfc5dad96611e6952cc70aa6b07689c7ceeca89
test1 <test@test.com>
2018-06-04 16:31:39 +0800

    sss

--- src/main/java/my/p/cheehong.java
+++ src/main/java/my/p/cheehong.java
@@ -1,11 +1,11 @@
 package my.p;
 
-public class HAHA {
+public class cheehong {
 	public static void main(String agrs[]){
 		System.out.println("chee hong sha bi");
 		System.out.println("chee hong sha bi");
 		System.out.println("chee hong sha bi");
 		
-		System.out.println("lol");
+		System.out.println("lol sha bi");
 	}
 }
