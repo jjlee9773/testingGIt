bdab5490f8fd90a85cdc6f32ea4bf7b7d88646cf
haha <haha@gmail.com>
2018-06-04 11:54:04 +0800

    zz

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -10,12 +10,19 @@ public class mingliang {
 		System.out.println("don't play play!!!wdcwv!!!");
 	}
 	
-	public void hiMingLiang111(){}
-}
+	public void hiMingLiang111(){
+		
+		System.out.println("bobo jj");
+	}
 	
-	public void hiMingLiang112(){}
+	public void hiMingLiang112(){
+		System.out.println("bobo ML");
+	}
 	
-	public void hiMingLiang113(){}
+	public void hiMingLiang113(){
+		System.out.println("bobo j");
+	}
 	
 	public void hiMingLiang114(){}
 }
+	
