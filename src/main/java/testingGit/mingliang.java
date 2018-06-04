2b71c158d17df832116ded187b5a0e0148b5f41a
ziyao.ng <ziyao.ng@gswhouse.com>
2018-06-04 11:48:08 +0800

    asda

--- src/main/java/testingGit/mingliang.java
+++ src/main/java/testingGit/mingliang.java
@@ -3,10 +3,19 @@ package testingGit;
 public class mingliang {
 	public void hiMingLiang(){
 		System.out.println("don't play play!!!!!!");
-
-		System.out.println("don't play play!!!!!");
+		System.out.println("don't play plawefwey!!!!!");
+		System.out.println("don't play playwefwef!!!!!");
+		System.out.println("don't play play!!!!!!zzzzzzzz");
+		System.out.println("don't play play!!wefwef!!!");
+		System.out.println("don't play play!!!wdcwv!!!");
 		System.out.println("don't play play!!!!!!");
 	}
 	
 	public void hiMingLiang111(){}
+	
+	public void hiMingLiang112(){}
+	
+	public void hiMingLiang113(){}
+	
+	public void hiMingLiang114(){}
 }
