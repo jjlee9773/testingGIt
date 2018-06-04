de8cf707351c79ec095c1934b702281bceacd228
JJ <kokhau.lee@gswhouse.com>
2018-06-04 12:32:02 +0800

    Revert "wewew"
    This reverts commit 572688e9f93a0c5b1df0d930077ef0b41907c7ea.

--- src/main/java/testingGit/testingMingLiang.java
+++ src/main/java/testingGit/testingMingLiang.java
@@ -4,8 +4,6 @@ public class testingMingLiang {
 	public void hiMingLiang(){}
 	public void cheeHong(){
 		System.out.println("swsw 11oooo");
-		System.out.println("hi cheeHong cb zaivvv");
-		System.out.println("hi cheeHong cb zaivvv");
 	}
 
 	public void cheeHong2(){}
