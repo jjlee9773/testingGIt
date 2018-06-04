79cd92cca458d9f5c3f27df1386b9d22e5e44b21
JJ <kokhau.lee@gswhouse.com>
2018-06-04 16:55:29 +0800

    sddsds

--- src/main/java/my/p/hiJiaTiong.java
+++ src/main/java/my/p/hiJiaTiong.java
@@ -5,7 +5,7 @@ public class hiJiaTiong {
 	void eat(){}
 	void sleep(){}
 	void wakeUp(){}
-	void fuck(){}
+	void fuckZiYao(){}
 	void play(){}
 	void makeBaby2(){}
 	void playWater(){}
--- src/main/java/my/p/keKe.java
+++ src/main/java/my/p/keKe.java
@@ -4,7 +4,12 @@ public class keKe {
 	
 	public static void main(String agr[]){
 		System.out.print("KEKE IS WHO");
+<<<<<<< Upstream, based on branch 'master' of https://github.com/jjlee9773/testingGIt.git
 		System.out.print("KEKEKEKEKEKEKE whahahahahahahahahaha");
+=======
+		System.out.print("KEKEKEKEKEKEKE");
+		System.out.print("KEKEKEKEKEKEKE");
+>>>>>>> efc58a4 sddsds
 	}
 
 }
