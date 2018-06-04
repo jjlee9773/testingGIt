8b67410b387d09e604dc51e06fa9b5ac5e55cf4a
JJ <kokhau.lee@gswhouse.com>
2018-06-04 12:51:35 +0800

    ssss

--- src/main/java/testingGit/testingMingLiang.java
+++ src/main/java/testingGit/testingMingLiang.java
@@ -4,15 +4,16 @@ public class testingMingLiang {
 	public void hiMingLiang(){}
 	public void cheeHong(){
 		System.out.println("swsw 11oooo");
+		System.out.println("hi mingliang cb zaivvv");
 	}
 
 	public void cheeHong2(){}
 
-	public void jj(){
-		System.out.println("hi jj cb zaivvv");
-		System.out.println("hi jj cb zaivvv");
-		System.out.println("hi jj cb zaivvv");
-		System.out.println("hi jj cb zaivvv");
-		System.out.println("hi jj cb zaivvv");
+	public void mingliang(){
+		System.out.println("hi mingliang cb zaivvv");
+		System.out.println("hi mingliang cb zaivvv");
+		System.out.println("hi mingliang cb zaivvv");
+		System.out.println("hi mingliang cb zaivvv");
+		System.out.println("hi mingliang cb zaivvv");
 	}
 }
