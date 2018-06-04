28428bde03f9a3bc5f3753e1401b1faeab34de0a
haha <haha@gmail.com>
2018-06-04 11:08:33 +0800

    jj

--- src/main/java/my/p/HAHA.java
+++ src/main/java/my/p/HAHA.java
@@ -2,6 +2,8 @@ package my.p;
 
 public class HAHA {
 	public static void main(String agrs[]){
+		System.out.println("jj sha bi");
+		System.out.println("chee hong sha bi");
 		System.out.println("chee hong sha bi");
 	}
 }
