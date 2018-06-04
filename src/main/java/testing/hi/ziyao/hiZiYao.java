32ffa20f8292182cd52433119c82e37e70c8a9a8
haha <haha@gmail.com>
2018-06-04 11:11:04 +0800

    xx

--- src/main/java/testing/hi/ziyao/hiZiYao.java
+++ src/main/java/testing/hi/ziyao/hiZiYao.java
@@ -3,10 +3,11 @@ package testing.hi.ziyao;
 public class hiZiYao {
 	public void hiZiyao(){
 		System.out.println("testing hi zi yaoaa");
-		System.out.println("testing hi zi yao gg");
-		System.out.println("aa");
-		System.out.println("bb");
 		System.out.println("testing hi zi yao");
 		System.out.println("testing hi zi yao");
+		System.out.println("testing hi zi yaoaa");
+		System.out.println("testing hi zi yao");
+		System.out.println("testing hi zi yao");
+		System.out.println("testing hi zi yao111");
 	}
 }
