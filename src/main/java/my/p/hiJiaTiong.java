8e897ee3f4240f8127e868fd8b15ac64f4cf32d1
jiatong.leow <jiatong.leow@wiseyes.net>
2018-06-06 15:17:43 +0800

    remove method

--- src/main/java/my/p/hiJiaTiong.java
+++ src/main/java/my/p/hiJiaTiong.java
@@ -2,15 +2,4 @@ package my.p;
 
 public class hiJiaTiong {
 
-	void eat(){}
-	void sleep(){}
-	void wakeUp(){}
-	void watchThisAV_com(){}
-	void fuckJJ(){}
-	void lanjiao(){}
-	void play(){}
-	void makeBaby2(){}
-	void playWater(){}
-	void fuckSelf(){}
-	void jiatongMakeBaybyWithZiYao(){}
 }
