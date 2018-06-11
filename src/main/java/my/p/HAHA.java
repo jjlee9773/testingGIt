abd8cff3f77b4d55f0ad30a797870f0604f5839d
cheehong.chong <cheehong.chong@wiseyes.net>
2018-06-11 13:21:42 +0800

    p

--- src/main/java/my/p/HAHA.java
+++ src/main/java/my/p/HAHA.java
@@ -1,13 +1,29 @@
 package my.p;
 
 public class HAHA {
-	public static void main(String agrs[]){
-		System.out.println("chee hong sha bi");
-		System.out.println("chee hong sha bi");
-		System.out.println("chee hong sha bi cch");
-		
-		System.out.println("lol");
-		System.out.println("lol");j
+	public static void main(String agrs[]){fsdfsd
+		System.out.println("chehodsfdsfng sha bi");
+		System.out.println("chee hdstftrgtrftftfsdong sha bi");sdfsdfs
+		System.out.println("cdsfsdfsdfsd
 	}
-	void letYouHitError(){}sss
+	void letYouHitError(){}sssfsdfsd
+}
+void letYouHitError(){}sssfsdfsd
+}
+void letYouHitError(){}sssfhee  sha bi cch");
+		d
+		System.out.println("lodfdfdfl");fsdfsdf
+		System.out.println("ol");jdfsdfsd
+	}
+	void letYouHitError(){}sssfsdfsdfsdfsd
+}
+void letYouHitError(){}sssfsdfsd
+}
+void letYouHitError(){}sss
+}
+void letYouHitError(){}sssfsdfsd
+}
+void letYouHitError(){}sssfsdfsd
+}
+void letYouHitError(){}sss
 }
